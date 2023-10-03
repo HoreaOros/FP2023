d69289aa6d9975457b2838f8ad3a4f055cf7810b
Horea Oros <horea.oros@gmail.com>
2023-10-03 13:59:17 +0300

    add Collatz problem

--- 1003/1003/Program.cs
+++ 1003/1003/Program.cs
@@ -8,8 +8,37 @@ namespace _1003
 {
     internal class Program
     {
+        static void f(int n)
+        {
+            Console.Write($"{n} ");
+            while (n != 1)
+            {
+                if (n % 2 == 0)
+                {
+                    n /= 2;
+                }
+                else
+                {
+                    n = 3 * n + 1;
+                }
+                Console.Write($"{n} ");
+            }
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
+            //Console.WriteLine("Hello World!");
+            //int n; 
+            //n = int.Parse(Console.ReadLine());
+            //Console.WriteLine(n);
+
+
+            for (int i = 1; i <= 10000; i++)
+            {
+                f(i);
+            }
+            
+
         }
     }
 }
